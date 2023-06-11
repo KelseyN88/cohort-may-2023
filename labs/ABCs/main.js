0f0e30c82f9c3d3a508e63a89c7ba6e88c477c72
Kelsey Nagy <nagy8907@gmail.com>
2023-06-11 18:50:53 -0400

    ABCs mini lesson

---
+++ labs/ABCs/main.js
@@ -0,0 +1,34 @@
+let abcs = [
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r,','s','t','u','v','w','x','y','z'
+];
+
+let pulledLetters = []; // track number of letters pulled 
+
+
+const findLetter = () => {
+    let num = Math.floor(Math.random() * abcs.length - 1)
+    // console.log(num);
+
+    let returnedLtr = abcs[num]
+    abcs.splice(num,1)
+
+    pulledLetters.push(returnedLtr) // push to empty array
+
+    //logic would go above this before return
+    // return `Coming from findLetter function`
+    return returnedLtr.toUpperCase()
+}
+
+
+
+function getLetter() {
+
+    let ltr = findLetter()
+
+    let res = `Your letter is ${ltr} \nHere are the pulled letters:
+    ${pulledLetters}. \n Here is what remains ${abcs}`
+
+    // return "See the return"
+    return res
+
+}
