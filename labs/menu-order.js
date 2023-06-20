e3c7626bb02487d2443f2961e357834fc6441cab
Kelsey Nagy <nagy8907@gmail.com>
2023-06-20 18:08:03 -0400

    lab

---
+++ labs/menu-order.js
@@ -0,0 +1,20 @@
+// Your code here, reference the instructions if you're unsure how to start
+
+let menu = {
+    burger: 5.55,
+    fries: 3.50,
+    shake: 1.11,
+    salad: 4.25
+  }
+  
+  function order(orderPlaced) {
+    let orderSplit = []
+    orderSum = 0
+  
+    for (item of orderSplit) {
+      orderSum = orderSum + this.price
+      orderSplit.push()
+    }
+  }
+
+console.log(order(menu.burger));
